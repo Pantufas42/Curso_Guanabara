202845173a469a657fa3660d785e8041f0edb267
Erich Herbert Bauchspiess <pantufas.42@gmail.com>
2021-05-03 11:28:38 -0300

    Update ex025.py

--- ex025.py
+++ ex025.py
@@ -1,9 +1,9 @@
 n = str(input('Qual é o seu nome? ')).strip()
-#Meu jeito fedido:
+#Meu jeito burro:
 #n = str(input('Qual é o seu nome? ')).strip()
 #np = (n[0:].lower() == 'bauchspiess')
 #print('É da família? {}'.format(np))
-#Jeito ipeto do Guanabara:
+#Jeito esperto do Guanabara:
 n = str(input('Qual é o seu nome? ')).strip()
 print('Você é da família? {}'.format('bauchspiess' in n.lower))
 
